526abf6d02a8936df7d1130c9992b456b4592460
Krystian Skwierawski <bezi25251@wp.pl>
2022-03-25 09:36:31 +0100

    Add project template

---
+++ Template6.0/Project.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Project.Tests
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void Test1()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
