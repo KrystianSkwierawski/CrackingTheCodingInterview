ab6559be5357b7a2c29a56dfea1940165c7051a2
Krystian Skwierawski <bezi25251@wp.pl>
2022-04-01 16:30:58 +0200

    Add 2.1 solution

---
+++ 2. Linked Lists/2.1 RemoveDups/Project.Tests/NodeTests.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Project.Tests
+{
+    public class NodeTests
+    {
+        [Test]
+        public void ShouldRemoveDups()
+        {
+            //Arrange
+            LinkedList<int> linkedList = new();
+            linkedList.AddLast(1);
+            linkedList.AddLast(3);
+            linkedList.AddLast(7);
+            linkedList.AddLast(6);
+            linkedList.AddLast(1);
+            linkedList.AddLast(7);
+            linkedList.AddLast(1);
+            linkedList.AddLast(1);
+
+
+            //Act
+            linkedList.RemoveDups(linkedList.Head);
+
+            //Assert
+            LinkedList<int> expectedResult = new();
+            expectedResult.AddLast(1);
+            expectedResult.AddLast(3);
+            expectedResult.AddLast(7);
+            expectedResult.AddLast(6);
+
+            linkedList.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
---
+++ 2. Linked Lists/2.1 RemoveDups/Project/LinkedList.cs
@@ -0,0 +1,55 @@
+﻿namespace Project;
+
+public class LinkedList<T>
+{
+    public Node<T> Head { get; set; }
+
+    public void AddLast(T value)
+    {
+        if (Head is null)
+        {
+            Head = new Node<T>(value);
+            return;
+        }
+
+        Node<T> current = Head;
+
+        while (current.Next is not null)
+        {
+            current = current.Next;
+        }
+
+        current.Next = new Node<T>(value);
+    }
+
+    public void RemoveDups(Node<T> head)
+    {
+        if (head is null)
+            return;
+
+        Node<T> current = head;
+
+        while (current is not null)
+        {
+            Node<T> j = current;
+
+            while (j?.Next is not null)
+            {
+                if (j.Next.Value.Equals(current.Value))
+                {
+                    j.Next = j.Next.Next;
+                    continue;
+                }
+
+                if (!(j.Next.Value.Equals(current.Value)))
+                {
+                    j = j.Next;
+                    continue;
+                }
+            }
+
+            current = current.Next;
+        }
+    }
+}
+
