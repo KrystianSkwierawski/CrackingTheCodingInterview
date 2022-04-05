bb0a07a38d97bf295fd878540a480585acd7502f
Krystian Skwierawski <bezi25251@wp.pl>
2022-04-05 12:46:12 +0200

    Update 1.1

--- 1. Arrays and Strings/1.1 Is Unique/Project/UniqueCharacterChecker.cs
+++ 1. Arrays and Strings/1.1 Is Unique/Project/UniqueCharacterChecker.cs
@@ -9,18 +9,16 @@ public class UniqueCharacterChecker : IUniqueCharacterChecker
         if(String.IsNullOrEmpty(text))
             throw new ArgumentNullException();
 
-        char[] textArr = text.ToCharArray();
-
-        for (int i = 0; i < textArr.Length; i++)
+        for (int i = 0; i < text.Length; i++)
         {
             int numberOfCurrentCharOccurrences = 0;
 
-            foreach (var @char in textArr)
+            for (int j = 0; j < text.Length; j++)
             {
-                if (@char == textArr[i] && numberOfCurrentCharOccurrences > 0)
+                if (text.Substring(j, 1) == text.Substring(i, 1) && numberOfCurrentCharOccurrences > 0)
                     return false;
 
-                if (@char == textArr[i])
+                if (text.Substring(j, 1) == text.Substring(i, 1))
                     numberOfCurrentCharOccurrences++;
             }
         }
